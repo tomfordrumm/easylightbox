980b09f0c91868390c392f4615be2b1e3185a949
Tomfordrumm <tomfordrumm@gmail.com>
2013-09-26 23:52:59 +0400

    add some fitches

--- app/code/local/SnowCommerce/IcePop/Helper/Data.php
+++ app/code/local/SnowCommerce/IcePop/Helper/Data.php
@@ -8,4 +8,36 @@
  */ 
 class SnowCommerce_IcePop_Helper_Data extends Mage_Core_Helper_Abstract {
 
+    const LOGO_WIDTH = 'icepop/img/width_logo';
+    const LOGO_HEIGHT = 'icepop/img/height_logo';
+    const BANNER_WIDTH = 'icepop/img/width_banner';
+    const BANNER_HEIGHT = 'icepop/img/height_banner';
+
+    public function checkBanner($dirImg,$imgName){
+            $x = Mage::getStoreConfig(self::BANNER_WIDTH);
+            $y = Mage::getStoreConfig(self::BANNER_HEIGHT);
+            $dir = Mage::getBaseDir('media').DS.'icepopresized'.DS;
+            $imageObj = new Varien_Image($dirImg);
+            $imageObj->constrainOnly(true);
+            $imageObj->keepAspectRatio(true);
+            $imageObj->keepFrame(false);
+            $imageObj->backgroundColor(array('255','255','255'));
+            $imageObj->keepTransparency(true);
+            $imageObj->resize($x, $y);
+            $imageObj->save($dir.$imgName);
+    }
+
+    public function checkLogo($dirImg,$imgName){
+        $x = Mage::getStoreConfig(self::LOGO_WIDTH);
+        $y = Mage::getStoreConfig(self::LOGO_HEIGHT);
+        $dir = Mage::getBaseDir('media').DS.'icepopresized'.DS;
+        $imageObj = new Varien_Image($dirImg);
+        $imageObj->constrainOnly(true);
+        $imageObj->keepAspectRatio(true);
+        $imageObj->keepFrame(false);
+        $imageObj->backgroundColor(array('255','255','255'));
+        $imageObj->keepTransparency(true);
+        $imageObj->resize($x, $y);
+        $imageObj->save($dir.$imgName);
+    }
 }
--- app/code/local/SnowCommerce/IcePop/controllers/Adminhtml/LightboxController.php
+++ app/code/local/SnowCommerce/IcePop/controllers/Adminhtml/LightboxController.php
@@ -8,6 +8,12 @@
  */
 class SnowCommerce_IcePop_Adminhtml_LightboxController extends Mage_Adminhtml_Controller_Action{
 
+    const LOGO_WIDTH = 'icepop/img/width_logo';
+    const LOGO_HEIGHT = 'icepop/img/height_logo';
+    const BANNER_WIDTH = 'icepop/img/width_banner';
+    const BANNER_HEIGHT = 'icepop/img/height_banner';
+
+
     public function _initAction(){
         $this->loadLayout()
             ->_setActiveMenu('snowcommerce/easylb')
@@ -62,8 +68,13 @@ class SnowCommerce_IcePop_Adminhtml_LightboxController extends Mage_Adminhtml_Co
                     $path = Mage::getBaseDir('media') . DS;
 
                     $uploader->save($path, $_FILES['logo']['name']);
-
-                    $data['logo'] = $_FILES['logo']['name'];
+                    $size = getimagesize($path.$_FILES['logo']['name']);
+                    if (($size[0] > Mage::getStoreConfig(self::LOGO_WIDTH)) and ($size[1] > Mage::getStoreConfig(self::LOGO_HEIGHT))){
+                        Mage::helper('sc_icepop')->checkLogo($path.$_FILES['logo']['name'],$_FILES['logo']['name']);
+                        $data['logo'] = 'icepopresized'.DS.$_FILES['logo']['name'];
+                    } else {
+                        $data['logo'] = $_FILES['logo']['name'];
+                    }
                 } catch (Exception $e) {
 
                 }
@@ -75,6 +86,7 @@ class SnowCommerce_IcePop_Adminhtml_LightboxController extends Mage_Adminhtml_Co
                     unset($data['logo']);
             }
             if (isset($_FILES['banner']['name']) and (file_exists($_FILES['banner']['tmp_name']))) {
+//                var_dump($_FILES['banner']); exit;
                 try {
                     $uploader = new Varien_File_Uploader('banner');
                     $uploader->setAllowedExtensions(array('jpg', 'jpeg', 'gif', 'png')); // or pdf or anything
@@ -89,8 +101,13 @@ class SnowCommerce_IcePop_Adminhtml_LightboxController extends Mage_Adminhtml_Co
                     $path = Mage::getBaseDir('media') . DS;
 
                     $uploader->save($path, $_FILES['banner']['name']);
-
-                    $data['banner'] = $_FILES['banner']['name'];
+                    $size = getimagesize($path.$_FILES['banner']['name']);
+                    if (($size[0] > Mage::getStoreConfig(self::BANNER_WIDTH)) and ($size[1] > Mage::getStoreConfig(self::BANNER_HEIGHT))){
+                        Mage::helper('sc_icepop')->checkBanner($path.$_FILES['banner']['name'],$_FILES['banner']['name']);
+                        $data['banner'] = 'icepopresized'.DS.$_FILES['banner']['name'];
+                    } else {
+                        $data['banner'] = $_FILES['banner']['name'];
+                    }
                 } catch (Exception $e) {
 
                 }
